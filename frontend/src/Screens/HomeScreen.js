d56e8a7b054edfad6193fc728ca28f8435a14c7c
sushant <aryalsushant564@gmail.com>
2023-01-05 12:02:29 +0545

    Added home page with shopping items

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,15 +1,15 @@
 import React from "react";
 import products from "./../product";
 import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
 function HomeScreen() {
-  let count = 0;
   return (
     <div>
       <h1>Latest Product</h1>
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <h3>{product.name}</h3>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
