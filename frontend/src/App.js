b206b1d8a627378a2f2577f56452932d784e357e
sushant <aryalsushant564@gmail.com>
2023-01-05 13:33:41 +0545

    Added react router to link pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,20 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import HomeScreen from "./Screens/HomeScreen.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductScreen from "./Screens/ProductScreen.js";
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <Container>
-        <HomeScreen />
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+        </Routes>
       </Container>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,23 +4,30 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
   return (
     <header>
       <Navbar bg="info" variant="white" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">SA Shop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>SA Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart">
-                <ShoppingCartIcon />
-                Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <AccountCircleIcon />
-                LogIn
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <ShoppingCartIcon />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <AccountCircleIcon />
+                  LogIn
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
